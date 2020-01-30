44921d4dcf5c454cd02f903a3437ffb6b55a6e8d
InnaL <innalapteva72@gmail.com>
2020-01-30 13:00:46 +0500

    довбавлен launch screen

--- SB_HW1_2/ViewController.swift
+++ SB_HW1_2/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 20
+        redView.layer.cornerRadius = 70
+        redView.alpha = 0.3
+        yellowView.layer.cornerRadius = 70
+        yellowView.alpha = 0.3
+        greenView.layer.cornerRadius = 70
+        greenView.alpha = 0.3
+        
     }
+    
+    @IBAction func startButtonPressed() {
+       
+        if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+        } else if redView.alpha == 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if redView.alpha != 1 && yellowView.alpha == 1 && greenView.alpha != 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+        
+        }
+        
+    }
+    
 
 
-}
 
